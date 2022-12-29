aab30b899494b09b94c33a7d6d8c12d733a76d66
mcnica89 <mcnica89@gmail.com>
2022-12-28 22:07:37 -0500

    Update macropad.py

--- macropad.py
+++ macropad.py
@@ -1,15 +1,14 @@
+# SPDX-FileCopyrightText: Copyright (c) 2021 Kattni Rembor for Adafruit Industries
 #
-#Code for adafruit macropad games
-#
+# SPDX-License-Identifier: Unlicense
+"""
+Simpletest demo for MacroPad. Displays the key pressed, the relative position of the rotary
+encoder, and the state of the rotary encoder switch to the built-in display. Note that the key
+pressed line does not appear until a key is pressed.
+"""
 from adafruit_macropad import MacroPad
-#import math
-
-macropad = MacroPad()
-# See https://docs.circuitpython.org/projects/macropad/en/latest/api.html#adafruit_macropad.MacroPad.pixels
-# for how to use the MacroPad package.
-
-text_lines = macropad.display_text(text_scale=3) #title="MacroPad Info")
-
+import random
+#
 RAW_COLORS = [
 'WHITE', (255,255,255),
 'RED', (255,0,0),
@@ -22,40 +21,41 @@ RAW_COLORS = [
 
 COLOR_NAMES = RAW_COLORS[::2]
 COLORS = RAW_COLORS[1::2]
-print(COLORS)
+N_COLORS = len(COLORS)
+#
+macropad = MacroPad()
+# See https://docs.circuitpython.org/projects/macropad/en/latest/api.html#adafruit_macropad.MacroPad.pixels
+# for how to use the MacroPad package.
+text_lines = macropad.display_text(text_scale=3) #title="MacroPad Info")
+#
+KeyColor = [random.randrange(N_COLORS) for i in range(12)]
+KeyState = [True for i in range(12)]
+#
 
-#for i in range(12):
-#    macropad.pixels[i] = (255,32+10*i,0)
-    
-#while True:
-#    pass
-key_brightness = [0.5 for i in range(12)]
-key_state = [False for i in range(12)]
+Display = ""
 
-log2_brightness = 0
+def update():
+    text_lines[0].text = Display 
+    text_lines.show()
+    for i in range(12):
+        if KeyState[i]:
+            macropad.pixels[i] = COLORS[KeyColor[i]]
+        else:
+            macropad.pixels[i] = (0,0,0) 
 
-display = ""
+log2_brightness = -4 #macropad.encoder - 2
+macropad.pixels.brightness = 2**log2_brightness
+#text_lines[2].text = "Encoder switch: {}".format(macropad.encoder_switch)
+        
 
+
+update()
 while True:
     key_event = macropad.keys.events.get()
     if key_event and key_event.pressed:
-        key_num = key_event.key_number
-        key_state[key_num] = not(key_state[key_num])
-        display = COLOR_NAMES[key_num]
-        text_lines[0].text = display #"Br:{}".format(log2_brightness)
-        #text_lines[2].text = "Encoder switch: {}".format(macropad.encoder_switch)
-        text_lines.show()
-        for key_num in range(len(COLORS)):
-            if key_state[key_num]:
-                macropad.pixels[key_num] = COLORS[key_num]
-            else:
-                macropad.pixels[key_num] = (0,0,0) 
-    
-
-    
-    
-     
-    #text_lines[0].text = "{}".format(COLOR_NAMES[key_num])
-    log2_brightness = macropad.encoder - 2
-    macropad.pixels.brightness = 2**log2_brightness
+        n = key_event.key_number
+        KeyState[n] = not(KeyState[n])
+        Display = COLOR_NAMES[KeyColor[n]]
+        update()
+        
     
