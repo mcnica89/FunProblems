9bcce8572dcde7639195e2b72c79decafbbd10a0
mcnica89 <mcnica89@gmail.com>
2022-12-28 22:22:32 -0500

    Update macropad.py

--- macropad.py
+++ macropad.py
@@ -28,12 +28,20 @@ macropad = MacroPad()
 # for how to use the MacroPad package.
 text_lines = macropad.display_text(text_scale=3) #title="MacroPad Info")
 #
-KeyColor = [random.randrange(N_COLORS) for i in range(12)]
-KeyState = [True for i in range(12)]
 #
 
 Display = ""
+KeyColor = [None for i in range(12)]
+KeyState = [None for i in range(12)]
 
+
+def initialize():
+    global KeyColor, KeyState, Display
+    Display = ""
+    KeyColor = [random.randrange(N_COLORS) for i in range(12)]
+    KeyState = [True for i in range(12)]
+
+    
 def update():
     text_lines[0].text = Display 
     text_lines.show()
@@ -43,19 +51,33 @@ def update():
         else:
             macropad.pixels[i] = (0,0,0) 
 
-log2_brightness = -4 #macropad.encoder - 2
+def play_happy():
+    for i in range(4):
+        macropad.play_tone(300+150*i,0.25)
+
+log2_brightness = -5 #macropad.encoder - 2
 macropad.pixels.brightness = 2**log2_brightness
 #text_lines[2].text = "Encoder switch: {}".format(macropad.encoder_switch)
         
 
-
+initialize()
 update()
 while True:
     key_event = macropad.keys.events.get()
     if key_event and key_event.pressed:
         n = key_event.key_number
+        
+        #Toggle KeyState and set display to the key color
         KeyState[n] = not(KeyState[n])
         Display = COLOR_NAMES[KeyColor[n]]
+        
+        #Update all the keys
         update()
+        
+        #If all keys are out, then restart the game
+        if not any(KeyState):
+            play_happy()
+            initialize()
+            update()
         
     
